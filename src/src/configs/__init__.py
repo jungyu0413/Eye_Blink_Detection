969dc66ad817c644e39f024f1dbe3b39ad9ac392
Jungyu Lee <jungyu0413@gmail.com>
2025-04-01 21:34:15 +0900

    Create __init__.py

---
+++ src/src/configs/__init__.py
@@ -0,0 +1,8 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Written by Bin Xiao (Bin.Xiao@microsoft.com)
+# Modified by Jun Guo (jun.guo.chn@outlook.com)
+# ------------------------------------------------------------------------------
+
+from .default import _C as cfg
