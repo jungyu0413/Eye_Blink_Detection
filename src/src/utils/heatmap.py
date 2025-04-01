fb42ee64da282f7dbb9cace4c12eda10248a3907
Jungyu Lee <jungyu0413@gmail.com>
2025-04-01 21:32:09 +0900

    Create heatmap.py

---
+++ src/src/utils/heatmap.py
@@ -0,0 +1,13 @@
+"""Utility methods for generating and visualizing heatmaps."""
+import numpy as np
+
+
+def gaussian_2d(shape, centre, sigma=1.0):
+    """Generate heatmap with single 2D gaussian."""
+    # xs col
+    xs = np.arange(0.5, shape[1] + 0.5, step=1.0, dtype=np.float32)
+    # ys row
+    ys = np.expand_dims(np.arange(0.5, shape[0] + 0.5, step=1.0, dtype=np.float32), -1)
+    alpha = -0.5 / (sigma**2)
+    heatmap = np.exp(alpha * ((xs - centre[0])**2 + (ys - centre[1])**2))
+    return heatmap
